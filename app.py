7371f9680aa3858ae1e254aa5c61f64935934f5a
Harmanan Kohli <17681934+Harmanankohli@users.noreply.github.com>
2024-08-12 19:48:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -101,7 +101,7 @@ def generate_chars(input, length):
     return "An error occured. Please try with another song"
 
 # streamlit app
-st.title("Lyrics Prediction With LSTM")
+st.title("Lyrics Generation With LSTM")
 st.write("Note: Please keep in mind that the lyrics sometimes will not make any sense as the LSTM is trained on characters rather than words")
 input_text=st.text_input("Enter the sequence of characters of atleast 50 characters","I call you when I need you, my heart's on fire You come to me, come to me")
 lyrics_char_length = st.text_input("Enter the number of characters you want as output",50)
